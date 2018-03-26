12a5e6666ba791e2741f40ee1e3e5e98e157ff4b
unknown <tommyn.gre@gmail.com>
2018-03-25 20:12:34 -0400

    toggle movement

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,7 @@ function welcome() {
 var session = {
   sessionData: '',
   queryBuilder: function (q) {
-    var url = "https://api.giphy.com/v1/gifs/search?limit=10&offset=0&rating=G&lang=en";
+    var url = "https://api.giphy.com/v1/gifs/search?limit=3&offset=0&rating=G&lang=en";
     var key = "&api_key=Q3JTt1KN39pzl91g1Pyh7pBNzyX9XuPv";
     var query = "&q=" + q;
     url = url + key + query;
@@ -27,10 +27,11 @@ var session = {
     var results = $("<div class='r-container'>");
 
     r.data.forEach(element => {
+      console.log(element);
       var result = $("<div class='r-element'>");
       var html =
         `
-        <img class="gif-onload" src="${element.images.downsized_still.url}">
+        <img class="gif-onload" src="${element.images.fixed_width_small_still.url}" data-motion="${element.images.fixed_width_small.url}" data-still="${element.images.fixed_width_small_still.url}" data-switch="0">
         `;
       result.html(html);
       results.append(result);
@@ -42,9 +43,22 @@ var session = {
 
 $("#show-me").on("click", function () {
   var query = $("#user-input").val();
-  //console.log(query);
   session.queryBuilder(query);
 });
+
+$("#container").on("click", ".gif-onload", function(){
+  console.log(this); // returns div
+  console.log($(this).attr("src"));
+
+  if ($(this).attr("data-switch") == "0"){
+    $(this).attr("src",$(this).attr("data-motion"));
+    $(this).attr("data-switch","1");
+  } else {
+    $(this).attr("src",$(this).attr("data-still"));
+    $(this).attr("data-switch","0");
+  }
+})
+
 
 $(document).ready(function () {
   welcome();
