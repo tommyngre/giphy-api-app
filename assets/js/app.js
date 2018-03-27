551bc32a47707d63f31848bf6d3cf444ad833ddf
unknown <tommyn.gre@gmail.com>
2018-03-26 22:32:30 -0400

    topics, start on style

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,18 @@
-function welcome() {
-  console.log("welcome fool")
-}
+var topics = ["spongebob","patrick","squidward","sandy cheeks","howard blandy","mr. krabs","mrs. puff","pearl krabs","doodlebob"];
+
 
 var session = {
-  sessionData: '',
+  search: '',
+  searches: [],
+  loadButtons: function(){
+    topics.forEach(topic => {
+      var button = $("<button class='col-md-2 btn btn-info btn-lg'>");
+      button.text(topic);
+      $("#topic-wrapper").append(button);
+    })
+  },
   queryBuilder: function (q) {
-    var url = "https://api.giphy.com/v1/gifs/search?limit=3&offset=0&rating=G&lang=en";
+    var url = "https://api.giphy.com/v1/gifs/search?limit=3&offset=0&lang=en";
     var key = "&api_key=Q3JTt1KN39pzl91g1Pyh7pBNzyX9XuPv";
     var query = "&q=" + q;
     url = url + key + query;
@@ -25,6 +32,9 @@ var session = {
   },
   drawGifs: function (r) {
     var results = $("<div class='r-container'>");
+    var resultsLabel = $("<h2 class='results-label text-center'>")
+    resultsLabel.text(session.search);
+    results.append(resultsLabel);
 
     r.data.forEach(element => {
       console.log(element);
@@ -32,24 +42,36 @@ var session = {
       var html =
         `
         <img class="gif-onload" src="${element.images.fixed_width_small_still.url}" data-motion="${element.images.fixed_width_small.url}" data-still="${element.images.fixed_width_small_still.url}" data-switch="0">
+        <div class="img-rating">
+          <span class="rating-label">rated</span>
+          <span class="rating">${element.rating}</span>
+        </div>
         `;
       result.html(html);
       results.append(result);
     });
 
-    $("#container").append(results);
+    $("#container").prepend(results);
   }
 }
 
-$("#show-me").on("click", function () {
+$("#show").on("click", function () {
   var query = $("#user-input").val();
+  //set session vars
+  session.search = query;
+  session.searches.push(query);
   session.queryBuilder(query);
 });
 
-$("#container").on("click", ".gif-onload", function(){
-  console.log(this); // returns div
-  console.log($(this).attr("src"));
+$("#topic-wrapper").on("click", ".btn", function(){
+  var query = $(this).html();
+  session.search = query;
+  session.searches.push(query);
+  session.queryBuilder(query);
+})
 
+$("#container").on("click", ".gif-onload", function(){
+  //console.log(this); // returns div
   if ($(this).attr("data-switch") == "0"){
     $(this).attr("src",$(this).attr("data-motion"));
     $(this).attr("data-switch","1");
@@ -59,7 +81,6 @@ $("#container").on("click", ".gif-onload", function(){
   }
 })
 
-
 $(document).ready(function () {
-  welcome();
+  session.loadButtons();
 })
